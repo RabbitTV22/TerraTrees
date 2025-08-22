0e5fc95879079f7d7d75137f4f077f1eba327310
RabbitTV22 <154371299+RabbitTV22@users.noreply.github.com>
2025-08-21 20:50:50 -0400

    Enhanced biome list command

--- src/main/java/me/rabbittv/terraTrees/commands/BiomeListCommand.java
+++ src/main/java/me/rabbittv/terraTrees/commands/BiomeListCommand.java
@@ -2,17 +2,21 @@ package me.rabbittv.terraTrees.commands;
 
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
 import com.dfsek.terra.api.config.ConfigPack;
 import com.dfsek.terra.api.world.WritableWorld;
 import com.dfsek.terra.api.world.biome.Biome;
 import com.dfsek.terra.bukkit.world.BukkitServerWorld;
 import me.rabbittv.terraTrees.TerraTrees;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @CommandAlias("terra")
 public class BiomeListCommand extends BaseCommand {
@@ -24,13 +28,33 @@ public class BiomeListCommand extends BaseCommand {
     }
 
     @Subcommand("listbiomes")
+    @Description("List all terra biomes in the current world. Also prints to plugins/TerraTrees/biome_list.txt")
     @CommandPermission("terratrees.listbiomes")
-    public void listBiomes(CommandSender sender) {
+    public void listBiomes(CommandSender sender) throws IOException {
         WritableWorld world = new BukkitServerWorld(Bukkit.getWorld(plugin.settings.getString("debug-world-name", "world")));
         ConfigPack pack = world.getPack();
+        List<String> biome_names = new ArrayList<>();
+        List<String> biome_names_raw = new ArrayList<>();
         pack.getRegistry(Biome.class).forEach((biome) -> {
-           sender.sendMessage(biome.toString());
+           String biome_name = biome.toString().toLowerCase()
+                   .replace("{biome:", "")
+                   .replace("}", "");
+           biome_names.add("terra:" + pack.getID().toLowerCase() + "/" + pack.getID().toLowerCase() + "/" + biome_name);
+           biome_names_raw.add(biome_name);
         });
+        Collections.sort(biome_names);
+        Collections.sort(biome_names_raw);
+        FileWriter writer = new FileWriter(new File(plugin.getDataFolder(), "biome_list.txt"));
+        FileWriter writer_raw = new FileWriter(new File(plugin.getDataFolder(), "biome_list_raw.txt"));
+        for (String biome_name : biome_names) {
+            sender.sendMessage(biome_name);
+            writer.write(biome_name + System.lineSeparator());
+        }
+        writer.close();
+        for (String biome_name : biome_names_raw) {
+            writer_raw.write(biome_name + System.lineSeparator());
+        }
+        writer_raw.close();
     }
 
 }
