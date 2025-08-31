f911c328eaa73478b4de4936b7d01dc65c2746a9
RabbitTV22 <154371299+RabbitTV22@users.noreply.github.com>
2025-08-30 21:40:13 -0400

    done for now

--- src/main/kotlin/me/rabbittv/terraTrees/TerraTrees.kt
+++ src/main/kotlin/me/rabbittv/terraTrees/TerraTrees.kt
@@ -3,21 +3,26 @@ package me.rabbittv.terraTrees
 import co.aikar.commands.PaperCommandManager
 import com.dfsek.terra.api.config.ConfigPack
 import com.dfsek.terra.api.structure.Structure
+import com.dfsek.terra.api.world.WritableWorld
 import com.dfsek.terra.bukkit.world.BukkitServerWorld
+import me.rabbittv.terraTrees.commands.BiomeListCommand
 import org.bukkit.Bukkit
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.plugin.java.JavaPlugin
 
 class TerraTrees : JavaPlugin(){
-    public final val settings = config.getConfigurationSection("settings")!!
-    public final val structures = config.getConfigurationSection("structures")!!
+    lateinit var settings: ConfigurationSection
+    lateinit var structures: ConfigurationSection
 
     override fun onEnable() {
         val manager: PaperCommandManager = PaperCommandManager(this)
         saveDefaultConfig()
-      //  manager.registerCommand()
+        manager.registerCommand(BiomeListCommand(this))
+        settings = config.getConfigurationSection("settings")!!
+        structures = config.getConfigurationSection("structures")!!
         if (settings.getBoolean("debug")) {
             val bukkitWorld = Bukkit.getWorld(settings.getString("debug-world-name", "world").toString())!!
-            val world: BukkitServerWorld = BukkitServerWorld(bukkitWorld)
+            val world: WritableWorld = BukkitServerWorld(bukkitWorld)
             val pack: ConfigPack = world.pack
             pack.getRegistry(Structure::class.java).keys().forEach { key ->
                 logger.info("Available Structure: $key")
--- src/main/kotlin/me/rabbittv/terraTrees/commands/BiomeListCommand.kt
+++ src/main/kotlin/me/rabbittv/terraTrees/commands/BiomeListCommand.kt
@@ -1,4 +1,48 @@
 package me.rabbittv.terraTrees.commands
 
-class BiomeListCommand {
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.Subcommand
+import com.dfsek.terra.api.world.biome.Biome
+import com.dfsek.terra.bukkit.world.BukkitServerWorld
+import me.rabbittv.terraTrees.TerraTrees
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import java.io.File
+import java.io.FileWriter
+import java.util.Collections
+
+@CommandAlias("terra")
+class BiomeListCommand(private val plugin: TerraTrees) : BaseCommand() {
+   @Subcommand("listbiomes")
+   @Description("List all terra biomes in the current world. Also prints to plugins/TerraTrees/biome_list.txt")
+   @CommandPermission("terratrees.listbiomes")
+   fun listBiomes(sender: CommandSender) {
+       val world = BukkitServerWorld(Bukkit.getWorld(plugin.settings.getString("debug-world-name", "world")!!))
+       val pack = world.pack
+       val biome_names = ArrayList<String>()
+       val biome_names_raw = ArrayList<String>()
+       pack.getRegistry(Biome::class.java).forEach { biome ->
+           val biome_name = biome.toString().lowercase()
+               .replace("{biome:", "")
+               .replace("}", "")
+           biome_names.add("terra:${pack.id.lowercase()}/${pack.id.lowercase()}$biome_name")
+           biome_names_raw.add(biome_name)
+       }
+       Collections.sort(biome_names)
+       Collections.sort(biome_names_raw)
+       val writer = FileWriter(File(plugin.dataFolder, "biome_list.txt"))
+       val writer_raw = FileWriter(File(plugin.dataFolder, "biome_list_raw.txt"))
+       for (biome_name in biome_names_raw) {
+           sender.sendMessage(biome_name)
+           writer.write(biome_name + System.lineSeparator())
+       }
+       writer.close()
+       for (biome_name in biome_names_raw) {
+           writer_raw.write(biome_name + System.lineSeparator())
+       }
+       writer_raw.close()
+   }
 }
